f39b94bd3d2d138bcb101a8ae6f1a01c1d7a6e51
Baiju Achari <baiju.achari@baesystems.com>
2017-12-22 21:48:51 -0500

    initial version

---
+++ JAXB/JAXB/src/main/java/com/baiju/webservice/JAXB/App.java
@@ -0,0 +1,13 @@
+package com.baiju.webservice.JAXB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
